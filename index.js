0cb82ab96b7d5a7d10748c6b913f9f98d1690c96
John Clever <cleverjohn69@gmail.com>
2021-08-23 02:51:07 -0400

    update y delete suxxes

--- index.js
+++ index.js
@@ -10,18 +10,54 @@ var app = express();
 
 var tasks = []
 
+var surrogatekey = 1;
+
 app.get("/", (req, res, next) => {
     res.json("{ 'message': 'Tasks server online'}");
 });
 
 app.post("/tasks", jsonParser, (req, res, next) => {
-    req.body.id = tasks.length + 1;
+    req.body.id = surrogatekey++;
+    req.body.status = "PEDING";
     tasks.push(req.body);
     res.send("OK");
 });
 
 app.get("/tasks", (req, res, next) => {
     res.json(tasks);
+});
+
+app.get('/tasks/:tasksId', function(req,res){
+    const task = tasks.find(task => task.id = req.params.tasksId);
+    if (task) {
+        res.json(task);
+    } else {
+        res.sendStatus(404);
+    } 
+});
+
+app.put('/tasks/:taskId', jsonParser, function(req,res){
+    var task = tasks.find(task => task.id == req.params.taskId);
+    task.title = req.body.title;
+    task.detail = req.body.detail;
+
+    if (task) {
+        res.json(task);
+    } else {
+        res.status(404);
+    }
+});
+
+app.delete('/tasks/:tasksId', function(req,res){
+    var clean = tasks.findIndex(clean => clean.id == req.params.cleanId);
+
+    console.log("clean" + clean);
+    if (clean) {
+        tasks.splice(clean);
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(404);
+    }
 });
 
 app.listen(3000, () => {
